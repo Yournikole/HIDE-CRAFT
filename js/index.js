62ae31785b1dbe6453983b59dcffd426e5e82578
Yournikole <younikole@GMAIL.COM>
2026-02-17 21:32:47 +0300

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,29 @@
+document.addEventListener("DOMContentLoaded", function() {
+
+  // Подсветка подвала
+  const contactsLink = document.getElementById('contacts-link');
+  const footer = document.querySelector('footer');
+
+  if (contactsLink && footer) {
+    contactsLink.addEventListener('click', function(e) {
+      e.preventDefault();
+
+      footer.scrollIntoView({ behavior: 'smooth' });
+
+      footer.classList.remove('highlight');
+      void footer.offsetWidth; // перезапуск анимации
+      footer.classList.add('highlight');
+    });
+  }
+
+  // Бургер-меню
+  const burger = document.querySelector(".burger");
+  const menu = document.querySelector(".menu");
+
+  if (burger && menu) {
+    burger.addEventListener("click", function() {
+      menu.classList.toggle("active");
+    });
+  }
+
+});
